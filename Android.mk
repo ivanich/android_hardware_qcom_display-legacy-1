c5d7f9642e1dd4d50805fc6363c6a13bddcfc386
Jay Wu <cn.fyodor@gmail.com>
2013-01-29 13:18:56 +0800

    s/LIBLIGHT/LIBLIGHTS/ for compatibility with old JB configs

--- Android.mk
+++ Android.mk
@@ -2,7 +2,7 @@ ifeq ($(call is-vendor-board-platform,QCOM),true)
 
 display-hals := libgralloc libgenlock libcopybit
 display-hals += libhwcomposer liboverlay libqdutils libexternal libqservice
-ifneq ($(TARGET_PROVIDES_LIBLIGHT),true)
+ifneq ($(TARGET_PROVIDES_LIBLIGHTS),true)
 display-hals += liblight
 endif
 include $(call all-named-subdir-makefiles,$(display-hals))
