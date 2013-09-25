4d8324299f7cf256a214921eea56c923151e046d
Nicholas Flintham <nick@flinny.org>
2013-09-25 20:20:32 +0100

    No longer needed
    Change-Id: Ic551b01cadd52819103c572edda4d7fe63a90a84

--- common.mk
+++ common.mk
@@ -8,11 +8,6 @@ common_includes += hardware/qcom/display/libhwcomposer
 common_includes += hardware/qcom/display/libexternal
 common_includes += hardware/qcom/display/libqservice
 
-ifeq ($(TARGET_USES_POST_PROCESSING),true)
-    common_flags     += -DUSES_POST_PROCESSING
-    common_includes += $(TARGET_OUT_HEADERS)/pp/inc
-endif
-
 
 #Common libraries external to display HAL
 common_libs := liblog libutils libcutils libhardware
