CyanogenMod/android_hardware_qcom_display

hwc: Allow MDP composition for Secure playback
    Allow MDP composition for Secure playback.
    Handle non-secure scenarios like
      1. When secure mode is enabled, if HWC receive a non-secure video
         layer, fallback to GPU.
      2. When non-secure mode is enabled, if HWC receive a secure video
         layer, fallback to GPU.

Change-Id: I7fba414e4dfbe9a87de3ce1e5e07f601bbccd5bd

libhwcomposer/hwc_mdpcomp.cpp
@@ -253,14 +253,6 @@ bool MDPComp::isDoable(hwc_context_t *ctx,
         return false;
     }
 
-    if(isSecuring(ctx)) {
-        ALOGD_IF(isDebug(), "%s: MDP securing is active", __FUNCTION__);
-        return false;
-    }
-
-    if(ctx->mSecureMode)
-        return false;
-
     //Check for skip layers
     if(isSkipPresent(ctx, dpy)) {
         ALOGD_IF(isDebug(), "%s: Skip layers are present",__FUNCTION__);
@@ -292,15 +284,21 @@ bool MDPComp::isDoable(hwc_context_t *ctx,
         // 180 transforms. Fail for any transform involving 90 (90, 270).
         hwc_layer_1_t* layer = &list->hwLayers[i];
         private_handle_t *hnd = (private_handle_t *)layer->handle;
-
-        if(layer->transform & HWC_TRANSFORM_ROT_90 && !isYuvBuffer(hnd)) {
-            ALOGD_IF(isDebug(), "%s: orientation involved",__FUNCTION__);
-            return false;
-        }
-
-        if(!isYuvBuffer(hnd) && !isWidthValid(ctx,layer)) {
-            ALOGD_IF(isDebug(), "%s: Buffer is of invalid width",__FUNCTION__);
-            return false;
+        if(isYuvBuffer(hnd) ) {
+            if(isSecuring(ctx, layer)) {
+                ALOGD_IF(isDebug(), "%s: MDP securing is active", __FUNCTION__);
+                return false;
+            }
+        } else {
+            if(layer->transform & HWC_TRANSFORM_ROT_90) {
+                ALOGD_IF(isDebug(), "%s: orientation involved",__FUNCTION__);
+                return false;
+            }
+            if(!isWidthValid(ctx,layer)) {
+                ALOGD_IF(isDebug(), "%s: Buffer is of invalid width",
+                                    __FUNCTION__);
+                return false;
+            }
         }
     }
     return true;

ivanich/android_hardware_qcom_display-legacy-1

Conflicts:
	libhwcomposer/hwc_mdpcomp.cpp

libhwcomposer/hwc_mdpcomp.cpp
@@ -258,14 +258,6 @@ bool MDPComp::isDoable(hwc_context_t *ctx,
         return false;
     }
 
-    if(isSecuring(ctx)) {
-        ALOGD_IF(isDebug(), "%s: MDP securing is active", __FUNCTION__);
-        return false;
-    }
-
-    if(ctx->mSecureMode)
-        return false;
-
     //Check for skip layers
     if(isSkipPresent(ctx, dpy)) {
         ALOGD_IF(isDebug(), "%s: Skip layers are present",__FUNCTION__);
@@ -297,15 +289,21 @@ bool MDPComp::isDoable(hwc_context_t *ctx,
         // 180 transforms. Fail for any transform involving 90 (90, 270).
         hwc_layer_1_t* layer = &list->hwLayers[i];
         private_handle_t *hnd = (private_handle_t *)layer->handle;
-
-        if(layer->transform & HWC_TRANSFORM_ROT_90 && !isYuvBuffer(hnd)) {
-            ALOGD_IF(isDebug(), "%s: orientation involved",__FUNCTION__);
-            return false;
-        }
-
-        if(!isYuvBuffer(hnd) && !isValidDimension(ctx,layer)) {
-            ALOGD_IF(isDebug(), "%s: Buffer is of invalid width",__FUNCTION__);
-            return false;
+        if(isYuvBuffer(hnd) ) {
+            if(isSecuring(ctx, layer)) {
+                ALOGD_IF(isDebug(), "%s: MDP securing is active", __FUNCTION__);
+                return false;
+            }
+        } else {
+            if(layer->transform & HWC_TRANSFORM_ROT_90) {
+                ALOGD_IF(isDebug(), "%s: orientation involved",__FUNCTION__);
+                return false;
+            }
+            if(!isValidDimension(ctx,layer)) {
+                ALOGD_IF(isDebug(), "%s: Buffer is of invalid width",
+                                    __FUNCTION__);
+                return false;
+            }
         }
     }
     return true;